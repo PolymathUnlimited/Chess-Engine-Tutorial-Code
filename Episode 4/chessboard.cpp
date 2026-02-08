a92c40dfd9056633098eaaeaa9aebac78a1dc66c
Polymath Unlimited <sonorousclover6@gmail.com>
2026-02-07 20:26:39 -0700

    Contains chessboard class method implementations

--- Episode 4/chessboard.cpp
+++ Episode 4/chessboard.cpp
@@ -1,4 +1,5 @@
 #include "chessboard.h"
+#include "bitops.h"
 #include <math.h>
 
 using namespace std;
@@ -17,6 +18,11 @@ void Chessboard::setPiece(uint8_t piece, uint8_t square)
 	for (int i = 0; i < 12; ++i)
 		if (i == piece) stateStack[stackIndex].bitboards[i] |= mask;
 		else stateStack[stackIndex].bitboards[i] &= ~mask;
+}
+
+uint8_t Chessboard::turn()
+{
+	return stateStack[stackIndex].turn;
 }
 
 void Chessboard::move(const Move& move)
@@ -80,7 +86,7 @@ void Chessboard::move(const Move& move)
 	if (moveBoard & 0x1100000000000000) stateStack[stackIndex].WQC = false;
 	if (moveBoard & 0x0000000000000090) stateStack[stackIndex].BKC = false;
 	if (moveBoard & 0x0000000000000011) stateStack[stackIndex].BQC = false;
-	
+
 	// promotions
 	if (move.promotion != EMPTY)
 	{
@@ -98,11 +104,451 @@ void Chessboard::undo()
 	if (stackIndex > 0) --stackIndex;
 }
 
+void Chessboard::pseudoMoves(Move* moves, int& numMoves)
+{
+	numMoves = 0;
+
+	// construct some bitboards
+	uint64_t whiteBoard = 0;
+	uint64_t blackBoard = 0;
+	for (int i = 0; i < 6; ++i) whiteBoard |= stateStack[stackIndex].bitboards[i];
+	for (int i = 6; i < 12; ++i) blackBoard |= stateStack[stackIndex].bitboards[i];
+	uint64_t board = whiteBoard | blackBoard;
+
+	// white moves
+	if (stateStack[stackIndex].turn == WHITE)
+	{
+		// pawn moves
+		uint64_t bb = (stateStack[stackIndex].bitboards[WHITE_PAWN] >> 8) & ~board;
+		uint64_t doubles = ((bb & 0x0000ff0000000000) >> 8) & ~board;
+		uint8_t square;
+		while (bb)
+		{
+			square = lsbIndex(bb);
+			bb &= bb - 1;
+			uint8_t origin = square + 8;
+			if (square < 8)
+			{
+				moves[numMoves++] = { origin, square, WHITE_QUEEN };
+				moves[numMoves++] = { origin, square, WHITE_ROOK };
+				moves[numMoves++] = { origin, square, WHITE_BISHOP };
+				moves[numMoves++] = { origin, square, WHITE_KNIGHT };
+			}
+			else moves[numMoves++] = { origin, square, EMPTY };
+		}
+		while (doubles)
+		{
+			square = lsbIndex(doubles);
+			doubles &= doubles - 1;
+			uint8_t origin = square + 16;
+			moves[numMoves++] = { origin, square, EMPTY };
+		}
+
+		// pawn attacks
+		bb = ((stateStack[stackIndex].bitboards[WHITE_PAWN] & 0xfefefefefefefefe) >> 9) & (blackBoard | stateStack[stackIndex].passantTarget);
+		while (bb)
+		{
+			square = lsbIndex(bb);
+			bb &= bb - 1;
+			uint8_t origin = square + 9;
+			if (square < 8)
+			{
+				moves[numMoves++] = { origin, square, WHITE_QUEEN };
+				moves[numMoves++] = { origin, square, WHITE_ROOK };
+				moves[numMoves++] = { origin, square, WHITE_BISHOP };
+				moves[numMoves++] = { origin, square, WHITE_KNIGHT };
+			}
+			else moves[numMoves++] = { origin, square, EMPTY };
+		}
+		bb = ((stateStack[stackIndex].bitboards[WHITE_PAWN] & 0x7f7f7f7f7f7f7f7f) >> 7) & (blackBoard | stateStack[stackIndex].passantTarget);
+		while (bb)
+		{
+			square = lsbIndex(bb);
+			bb &= bb - 1;
+			uint8_t origin = square + 7;
+			if (square < 8)
+			{
+				moves[numMoves++] = { origin, square, WHITE_QUEEN };
+				moves[numMoves++] = { origin, square, WHITE_ROOK };
+				moves[numMoves++] = { origin, square, WHITE_BISHOP };
+				moves[numMoves++] = { origin, square, WHITE_KNIGHT };
+			}
+			else moves[numMoves++] = { origin, square, EMPTY };
+		}
+
+		// knight moves
+		bb = stateStack[stackIndex].bitboards[WHITE_KNIGHT];
+		while (bb)
+		{
+			square = lsbIndex(bb);
+			bb &= bb - 1;
+
+			uint64_t knightMoves = knightAttacks[square] & ~whiteBoard;
+			uint8_t target;
+			while (knightMoves)
+			{
+				target = lsbIndex(knightMoves);
+				knightMoves &= knightMoves - 1;
+				moves[numMoves++] = { square, target, EMPTY };
+			}
+		}
+
+		// diagonalMoves
+		bb = stateStack[stackIndex].bitboards[WHITE_BISHOP] | stateStack[stackIndex].bitboards[WHITE_QUEEN];
+		while (bb)
+		{
+			square = lsbIndex(bb);
+			bb &= bb - 1;
+
+			uint64_t diagonalMoves = 0;
+			for (int i = 0; i < 4; ++i)
+			{
+				diagonalMoves |= diagonalRays[square][i];
+				uint64_t blockers = board & diagonalRays[square][i];
+				if (blockers)
+				{
+					uint8_t blockerIndex;
+					if (i == 0 || i == 3) blockerIndex = lsbIndex(blockers);
+					else blockerIndex = msbIndex(blockers);
+					diagonalMoves &= ~diagonalRays[blockerIndex][i];
+				}
+			}
+			diagonalMoves &= ~whiteBoard;
+
+			uint8_t target;
+			while (diagonalMoves)
+			{
+				target = lsbIndex(diagonalMoves);
+				diagonalMoves &= diagonalMoves - 1;
+				moves[numMoves++] = { square, target, EMPTY };
+			}
+		}
+
+		// cardinal moves
+		bb = stateStack[stackIndex].bitboards[WHITE_ROOK] | stateStack[stackIndex].bitboards[WHITE_QUEEN];
+		while (bb)
+		{
+			square = lsbIndex(bb);
+			bb &= bb - 1;
+
+			uint64_t cardinalMoves = 0;
+			for (int i = 0; i < 4; ++i)
+			{
+				cardinalMoves |= cardinalRays[square][i];
+				uint64_t blockers = board & cardinalRays[square][i];
+				if (blockers)
+				{
+					uint8_t blockerIndex;
+					if (i == 0 || i == 2) blockerIndex = lsbIndex(blockers);
+					else blockerIndex = msbIndex(blockers);
+					cardinalMoves &= ~cardinalRays[blockerIndex][i];
+				}
+			}
+			cardinalMoves &= ~whiteBoard;
+
+			uint8_t target;
+			while (cardinalMoves)
+			{
+				target = lsbIndex(cardinalMoves);
+				cardinalMoves &= cardinalMoves - 1;
+				moves[numMoves++] = { square, target, EMPTY };
+			}
+		}
+
+		// king moves
+		square = lsbIndex(stateStack[stackIndex].bitboards[WHITE_KING]);
+		bb = kingAttacks[square] & ~whiteBoard;
+		while (bb)
+		{
+			uint8_t target = lsbIndex(bb);
+			bb &= bb - 1;
+			moves[numMoves++] = { square, target, EMPTY };
+		}
+
+		// castling
+		if (stateStack[stackIndex].WKC && !(board & 0x6000000000000000) && !isAttacked(60, BLACK) && !isAttacked(61, BLACK)) moves[numMoves++] = { 60, 62, EMPTY };
+		if (stateStack[stackIndex].WQC && !(board & 0x0e00000000000000) && !isAttacked(60, BLACK) && !isAttacked(59, BLACK)) moves[numMoves++] = { 60, 58, EMPTY };
+	}
+
+	// black moves
+	else
+	{
+		// pawn moves
+		uint64_t bb = (stateStack[stackIndex].bitboards[BLACK_PAWN] << 8) & ~board;
+		uint64_t doubles = ((bb & 0x0000000000ff0000) << 8) & ~board;
+		uint8_t square;
+		while (bb)
+		{
+			square = lsbIndex(bb);
+			bb &= bb - 1;
+			uint8_t origin = square - 8;
+			if (square >= 56)
+			{
+				moves[numMoves++] = { origin, square, BLACK_QUEEN };
+				moves[numMoves++] = { origin, square, BLACK_ROOK };
+				moves[numMoves++] = { origin, square, BLACK_BISHOP };
+				moves[numMoves++] = { origin, square, BLACK_KNIGHT };
+			}
+			else moves[numMoves++] = { origin, square, EMPTY };
+		}
+		while (doubles)
+		{
+			square = lsbIndex(doubles);
+			doubles &= doubles - 1;
+			uint8_t origin = square - 16;
+			moves[numMoves++] = { origin, square, EMPTY };
+		}
+
+		// pawn attacks
+		bb = ((stateStack[stackIndex].bitboards[BLACK_PAWN] & 0xfefefefefefefefe) << 7) & (whiteBoard | stateStack[stackIndex].passantTarget);
+		while (bb)
+		{
+			square = lsbIndex(bb);
+			bb &= bb - 1;
+			uint8_t origin = square - 7; if (square >= 56)
+			{
+				moves[numMoves++] = { origin, square, BLACK_QUEEN };
+				moves[numMoves++] = { origin, square, BLACK_ROOK };
+				moves[numMoves++] = { origin, square, BLACK_BISHOP };
+				moves[numMoves++] = { origin, square, BLACK_KNIGHT };
+			}
+			else moves[numMoves++] = { origin, square, EMPTY };
+		}
+		bb = ((stateStack[stackIndex].bitboards[BLACK_PAWN] & 0x7f7f7f7f7f7f7f7f) << 9)& (whiteBoard | stateStack[stackIndex].passantTarget);
+		while (bb)
+		{
+			square = lsbIndex(bb);
+			bb &= bb - 1;
+			uint8_t origin = square - 9; if (square >= 56)
+			{
+				moves[numMoves++] = { origin, square, BLACK_QUEEN };
+				moves[numMoves++] = { origin, square, BLACK_ROOK };
+				moves[numMoves++] = { origin, square, BLACK_BISHOP };
+				moves[numMoves++] = { origin, square, BLACK_KNIGHT };
+			}
+			else moves[numMoves++] = { origin, square, EMPTY };
+		}
+
+		// knight moves
+		bb = stateStack[stackIndex].bitboards[BLACK_KNIGHT];
+		while (bb)
+		{
+			square = lsbIndex(bb);
+			bb &= bb - 1;
+
+			uint64_t knightMoves = knightAttacks[square] & ~blackBoard;
+			uint8_t target;
+			while (knightMoves)
+			{
+				target = lsbIndex(knightMoves);
+				knightMoves &= knightMoves - 1;
+				moves[numMoves++] = { square, target, EMPTY };
+			}
+		}
+
+		// diagonalMoves
+		bb = stateStack[stackIndex].bitboards[BLACK_BISHOP] | stateStack[stackIndex].bitboards[BLACK_QUEEN];
+		while (bb)
+		{
+			square = lsbIndex(bb);
+			bb &= bb - 1;
+
+			uint64_t diagonalMoves = 0;
+			for (int i = 0; i < 4; ++i)
+			{
+				diagonalMoves |= diagonalRays[square][i];
+				uint64_t blockers = board & diagonalRays[square][i];
+				if (blockers)
+				{
+					uint8_t blockerIndex;
+					if (i == 0 || i == 3) blockerIndex = lsbIndex(blockers);
+					else blockerIndex = msbIndex(blockers);
+					diagonalMoves &= ~diagonalRays[blockerIndex][i];
+				}
+			}
+			diagonalMoves &= ~blackBoard;
+
+			uint8_t target;
+			while (diagonalMoves)
+			{
+				target = lsbIndex(diagonalMoves);
+				diagonalMoves &= diagonalMoves - 1;
+				moves[numMoves++] = { square, target, EMPTY };
+			}
+		}
+
+		// cardinal moves
+		bb = stateStack[stackIndex].bitboards[BLACK_ROOK] | stateStack[stackIndex].bitboards[BLACK_QUEEN];
+		while (bb)
+		{
+			square = lsbIndex(bb);
+			bb &= bb - 1;
+
+			uint64_t cardinalMoves = 0;
+			for (int i = 0; i < 4; ++i)
+			{
+				cardinalMoves |= cardinalRays[square][i];
+				uint64_t blockers = board & cardinalRays[square][i];
+				if (blockers)
+				{
+					uint8_t blockerIndex;
+					if (i == 0 || i == 2) blockerIndex = lsbIndex(blockers);
+					else blockerIndex = msbIndex(blockers);
+					cardinalMoves &= ~cardinalRays[blockerIndex][i];
+				}
+			}
+			cardinalMoves &= ~blackBoard;
+
+			uint8_t target;
+			while (cardinalMoves)
+			{
+				target = lsbIndex(cardinalMoves);
+				cardinalMoves &= cardinalMoves - 1;
+				moves[numMoves++] = { square, target, EMPTY };
+			}
+		}
+
+		// king moves
+		square = lsbIndex(stateStack[stackIndex].bitboards[BLACK_KING]);
+		bb = kingAttacks[square] & ~blackBoard;
+		while (bb)
+		{
+			uint8_t target = lsbIndex(bb);
+			bb &= bb - 1;
+			moves[numMoves++] = { square, target, EMPTY };
+		}
+
+		// castling
+		if (stateStack[stackIndex].BKC && !(board & 0x0000000000000060) && !isAttacked(4, WHITE) && !isAttacked(5, WHITE)) moves[numMoves++] = { 4, 6, EMPTY };
+		if (stateStack[stackIndex].BQC && !(board & 0x000000000000000e) && !isAttacked(4, WHITE) && !isAttacked(3, WHITE)) moves[numMoves++] = { 4, 2, EMPTY };
+	}
+}
+
+bool Chessboard::isLegal(const Move& move)
+{
+	Move moves[218];
+	int numMoves;
+	pseudoMoves(moves, numMoves);
+
+	for (int i = 0; i < numMoves; ++i)
+		if (move == moves[i])
+		{
+			this->move(moves[i]);
+			bool illegal = turn() == WHITE ? isAttacked(blackKingSquare(), WHITE) : isAttacked(whiteKingSquare(), BLACK);
+			undo();
+			if (!illegal) return true;
+		}
+
+	return false;
+}
+
+bool Chessboard::isAttacked(uint8_t square, uint8_t color)
+{
+	if (color == BLACK)
+	{
+		uint64_t board = 0;
+		for (int i = 0; i < 12; ++i) board |= stateStack[stackIndex].bitboards[i];
+
+		// cardinally attacked
+		uint64_t attackers = stateStack[stackIndex].bitboards[BLACK_QUEEN] | stateStack[stackIndex].bitboards[BLACK_ROOK];
+		for (int i = 0; i < 4; ++i)
+		{
+			uint64_t blockers = cardinalRays[square][i] & board;
+			if (blockers & attackers)
+			{
+				uint8_t blockerIndex;
+				if (i == 0 || i == 2) blockerIndex = lsbIndex(blockers);
+				else blockerIndex = msbIndex(blockers);
+				if (attackers & (uint64_t(1) << blockerIndex)) return true;
+			}
+		}
+
+		// diagonally attacked
+		attackers = stateStack[stackIndex].bitboards[BLACK_QUEEN] | stateStack[stackIndex].bitboards[BLACK_BISHOP];
+		for (int i = 0; i < 4; ++i)
+		{
+			uint64_t blockers = diagonalRays[square][i] & board;
+			if (blockers & attackers)
+			{
+				uint8_t blockerIndex;
+				if (i == 0 || i == 3) blockerIndex = lsbIndex(blockers);
+				else blockerIndex = msbIndex(blockers);
+				if (attackers & (uint64_t(1) << blockerIndex)) return true;
+			}
+		}
+
+		// attacked by a knight
+		if (knightAttacks[square] & stateStack[stackIndex].bitboards[BLACK_KNIGHT]) return true;
+
+		// attacked by a pawn
+		if ((((stateStack[stackIndex].bitboards[BLACK_PAWN] & 0xfefefefefefefefe) << 7) | ((stateStack[stackIndex].bitboards[BLACK_PAWN] & 0x7f7f7f7f7f7f7f7f) << 9)) & (uint64_t(1) << square)) return true;
+	
+		// attacked by a king
+		if (kingAttacks[square] & stateStack[stackIndex].bitboards[BLACK_KING]) return true;
+	}
+	else if (color == WHITE)
+	{
+		uint64_t board = 0;
+		for (int i = 0; i < 12; ++i) board |= stateStack[stackIndex].bitboards[i];
+
+		// cardinally attacked
+		uint64_t attackers = stateStack[stackIndex].bitboards[WHITE_QUEEN] | stateStack[stackIndex].bitboards[WHITE_ROOK];
+		for (int i = 0; i < 4; ++i)
+		{
+			uint64_t blockers = cardinalRays[square][i] & board;
+			if (blockers & attackers)
+			{
+				uint8_t blockerIndex;
+				if (i == 0 || i == 2) blockerIndex = lsbIndex(blockers);
+				else blockerIndex = msbIndex(blockers);
+				if (attackers & (uint64_t(1) << blockerIndex)) return true;
+			}
+		}
+
+		// diagonally attacked
+		attackers = stateStack[stackIndex].bitboards[WHITE_QUEEN] | stateStack[stackIndex].bitboards[WHITE_BISHOP];
+		for (int i = 0; i < 4; ++i)
+		{
+			uint64_t blockers = diagonalRays[square][i] & board;
+			if (blockers & attackers)
+			{
+				uint8_t blockerIndex;
+				if (i == 0 || i == 3) blockerIndex = lsbIndex(blockers);
+				else blockerIndex = msbIndex(blockers);
+				if (attackers & (uint64_t(1) << blockerIndex)) return true;
+			}
+		}
+
+		// attacked by a knight
+		if (knightAttacks[square] & stateStack[stackIndex].bitboards[WHITE_KNIGHT]) return true;
+
+		// attacked by a pawn
+		if ((((stateStack[stackIndex].bitboards[WHITE_PAWN] & 0xfefefefefefefefe) >> 9) | ((stateStack[stackIndex].bitboards[WHITE_PAWN] & 0x7f7f7f7f7f7f7f7f) >> 7)) & (uint64_t(1) << square)) return true;
+
+		// attacked by a king
+		if (kingAttacks[square] & stateStack[stackIndex].bitboards[WHITE_KING]) return true;
+	}
+
+	return false;
+}
+
+uint8_t Chessboard::whiteKingSquare()
+{
+	return lsbIndex(stateStack[stackIndex].bitboards[WHITE_KING]);
+}
+
+uint8_t Chessboard::blackKingSquare()
+{
+	return lsbIndex(stateStack[stackIndex].bitboards[BLACK_KING]);
+}
+
 Chessboard::Chessboard()
 {
+	// allocate memory for the state stack
 	stateStack = new BoardState[1000];
 	stackIndex = 0;
 
+	// set up initial board state
 	stateStack[0].bitboards[WHITE_PAWN] = 0x00ff000000000000;
 	stateStack[0].bitboards[WHITE_KNIGHT] = 0x4200000000000000;
 	stateStack[0].bitboards[WHITE_BISHOP] = 0x2400000000000000;
@@ -124,6 +570,55 @@ Chessboard::Chessboard()
 	stateStack[0].passantTarget = 0;
 
 	stateStack[0].turn = WHITE;
+
+	// generate attack boards
+	for (int x1 = 0; x1 < 8; ++x1)
+		for (int y1 = 0; y1 < 8; ++y1)
+		{
+			uint8_t squareIndex = x1 + 8 * y1;
+
+			// knight attacks
+			knightAttacks[squareIndex] = 0;
+			if (x1 < 7 && y1 < 6) knightAttacks[squareIndex] |= uint64_t(1) << (x1 + 1 + 8 * (y1 + 2));
+			if (x1 < 6 && y1 < 7) knightAttacks[squareIndex] |= uint64_t(1) << (x1 + 2 + 8 * (y1 + 1));
+			if (x1 < 7 && y1 > 1) knightAttacks[squareIndex] |= uint64_t(1) << (x1 + 1 + 8 * (y1 - 2));
+			if (x1 > 1 && y1 < 7) knightAttacks[squareIndex] |= uint64_t(1) << (x1 - 2 + 8 * (y1 + 1));
+			if (x1 < 6 && y1 > 0) knightAttacks[squareIndex] |= uint64_t(1) << (x1 + 2 + 8 * (y1 - 1));
+			if (x1 > 0 && y1 < 6) knightAttacks[squareIndex] |= uint64_t(1) << (x1 - 1 + 8 * (y1 + 2));
+			if (x1 > 0 && y1 > 1) knightAttacks[squareIndex] |= uint64_t(1) << (x1 - 1 + 8 * (y1 - 2));
+			if (x1 > 1 && y1 > 0) knightAttacks[squareIndex] |= uint64_t(1) << (x1 - 2 + 8 * (y1 - 1));
+
+			// diagonal attack rays
+			diagonalRays[squareIndex][0] = 0;
+			for (int x2 = x1 + 1, y2 = y1 + 1; x2 < 8 && y2 < 8; ++x2, ++y2) diagonalRays[squareIndex][0] |= uint64_t(1) << (x2 + 8 * y2);
+			diagonalRays[squareIndex][1] = 0;
+			for (int x2 = x1 + 1, y2 = y1 - 1; x2 < 8 && y2 >= 0; ++x2, --y2) diagonalRays[squareIndex][1] |= uint64_t(1) << (x2 + 8 * y2);
+			diagonalRays[squareIndex][2] = 0;
+			for (int x2 = x1 - 1, y2 = y1 - 1; x2 >= 0 && y2 >= 0; --x2, --y2) diagonalRays[squareIndex][2] |= uint64_t(1) << (x2 + 8 * y2);
+			diagonalRays[squareIndex][3] = 0;
+			for (int x2 = x1 - 1, y2 = y1 + 1; x2 >= 0 && y2 < 8; --x2, ++y2) diagonalRays[squareIndex][3] |= uint64_t(1) << (x2 + 8 * y2);
+
+			// cardinal attack rays
+			cardinalRays[squareIndex][0] = 0;
+			for (int x2 = x1 + 1; x2 < 8; ++x2) cardinalRays[squareIndex][0] |= uint64_t(1) << (x2 + 8 * y1);
+			cardinalRays[squareIndex][1] = 0;
+			for (int x2 = x1 - 1; x2 >= 0; --x2) cardinalRays[squareIndex][1] |= uint64_t(1) << (x2 + 8 * y1);
+			cardinalRays[squareIndex][2] = 0;
+			for (int y2 = y1 + 1; y2 < 8; ++y2) cardinalRays[squareIndex][2] |= uint64_t(1) << (x1 + 8 * y2);
+			cardinalRays[squareIndex][3] = 0;
+			for (int y2 = y1 - 1; y2 >= 0; --y2) cardinalRays[squareIndex][3] |= uint64_t(1) << (x1 + 8 * y2);
+
+			// king attacks
+			kingAttacks[squareIndex] = 0;
+			if (x1 < 7 && y1 < 7) kingAttacks[squareIndex] |= uint64_t(1) << (x1 + 1 + 8 * (y1 + 1));
+			if (x1 < 7) kingAttacks[squareIndex] |= uint64_t(1) << (x1 + 1 + 8 * y1);
+			if (x1 < 7 && y1 > 0) kingAttacks[squareIndex] |= uint64_t(1) << (x1 + 1 + 8 * (y1 - 1));
+			if (y1 < 7) kingAttacks[squareIndex] |= uint64_t(1) << (x1 + 8 * (y1 + 1));
+			if (y1 > 0) kingAttacks[squareIndex] |= uint64_t(1) << (x1 + 8 * (y1 - 1));
+			if (x1 > 0 && y1 < 7) kingAttacks[squareIndex] |= uint64_t(1) << (x1 - 1 + 8 * (y1 + 1));
+			if (x1 > 0) kingAttacks[squareIndex] |= uint64_t(1) << (x1 - 1 + 8 * y1);
+			if (x1 > 0 && y1 > 0) kingAttacks[squareIndex] |= uint64_t(1) << (x1 - 1 + 8 * (y1 - 1));
+		}
 }
 
 Chessboard::~Chessboard()
